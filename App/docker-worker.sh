7bccfcd8e79a5aff02f8c30155b7d82e6e636833
Krzysztof Kukla <krzysztof.kukla@relativity.com>
2023-11-28 21:11:10 +0100

    docker file

--- App/docker-worker.sh
+++ App/docker-worker.sh
@@ -2,16 +2,12 @@ PLATFORM=linux/arm64
 IMAGE_NAME="trace-worker-image"
 TEMPORAL_HOST=172.18.0.4:7233
 
-echo "### image cleanup ###"
 docker ps -a
 docker stop t1 t2 t3 t4 t5
 docker rm t1 t2 t3 t4 t5
 docker images
 docker rmi $IMAGE_NAME
 
-echo "### docker build ###"
 docker build --platform=$PLATFORM -t $IMAGE_NAME -f TraceWorker/Dockerfile .
 
-echo "### docker save ###"
-docker save $IMAGE_NAME -o ~/Downloads/$IMAGE_NAME.img
 docker run --name=t1 -d --network temporal-network $IMAGE_NAME $TEMPORAL_HOST
