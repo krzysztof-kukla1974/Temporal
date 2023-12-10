acad94e5e2a93f5f4c9964baac310aee5faa5356
Krzysztof Kukla <krzysztof.kukla@relativity.com>
2023-12-10 12:57:36 +0100

    Fixes

--- App/docker-worker-remote.sh
+++ App/docker-worker-remote.sh
@@ -2,7 +2,7 @@ REMOTE_HOST="20.83.233.60"
 REMOTE_USER="superuser"
 PLATFORM="linux/amd64"
 IMAGE_NAME="trace-worker-image"
-TEMPORAL_HOST=172.18.0.4:7233
+TEMPORAL_HOST="172.19.0.4:7233"
 
 echo "### image cleanup ###"
 docker ps -a
@@ -18,8 +18,10 @@ echo "### docker save ###"
 docker save $IMAGE_NAME -o ~/Downloads/$IMAGE_NAME.img
 
 echo "### upload image onto remote machine ###"
-
 scp ~/Downloads/$IMAGE_NAME.img $REMOTE_USER@$REMOTE_HOST:~/Downloads
+echo "### upload client onto remote machine ###"
+tar -cvzf ~/Downloads/client.tar ~/Projects/Temporal/App/TraceClient/bin/Release/net6.0/*
+scp ~/Downloads/client.tar $REMOTE_USER@$REMOTE_HOST:~/Downloads
 ssh -tt $REMOTE_USER@$REMOTE_HOST << EOF
 
 docker ps -a
@@ -29,6 +31,10 @@ docker images
 docker rmi $IMAGE_NAME
 docker load -i ~/Downloads/$IMAGE_NAME.img
 docker run --name=t1 -d --network temporal-network $IMAGE_NAME $TEMPORAL_HOST
-
+rm -f ~/Downloads/client
+tar -xvf ~/Downloads/client.tar -C ~/Downloads
+cd ~/Downloads/Users/krzysztof.kukla/Projects/Temporal/App/TraceClient/bin/Release/net6.0
+dotnet TraceClient.dll $TEMPORAL_HOST 2023-01-01 2023-01-05
+docker logs t1 | grep Results
 exit
 EOF
